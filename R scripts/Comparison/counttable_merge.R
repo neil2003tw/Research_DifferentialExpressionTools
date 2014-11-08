df7d6db3a077cf6c5784e70a551af508ba78465e
neil2003tw <neil2003tw@gmail.com>
2014-11-08 14:08:24 +0800

    1.Remove old Profiler 2.Categorized script by usage 3.Add count table merge script

---
+++ R scripts/Comparison/counttable_merge.R
@@ -0,0 +1,15 @@
+
+##DElist<-DElist[!is.na(DElist$DE),]
+##test[is.na(test)]<-0
+
+data_name<-dir()
+condition<-substr(dir(),10,11)
+datas<-paste0('profile',condition)
+for(name in seq(1,length(dir()))){
+  assign(datas[name],read.csv(data_name[name],sep = ' ',col.names=c('Gene',condition[name])))
+}
+##list_profile<-lapply(ls(), function(x) if (class(get(x)) == "data.frame") get(x))
+list_profile<-lapply(ls(pattern = 'profile[AB][0-9]'), function(x) get(x))
+count_table<-Reduce(function(x, y) merge(x, y, all=TRUE), list_profile)
+count_table[is.na(count_table)]<-0
+

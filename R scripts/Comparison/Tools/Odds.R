984c4e231bdf8c06b270ad63b2b358416f16c9c0
neil2003tw <neil2003tw@gmail.com>
2015-01-14 18:00:55 +0800

    Odd modified

--- R scripts/Comparison/Tools/Odds.R
+++ R scripts/Comparison/Tools/Odds.R
@@ -38,7 +38,7 @@ counttable_merged<-function(directory){
 
   data_name<-dir()
   data_name<-data_name[grepl('profiled',data_name)]
-  condition<-substr(data_name,10,11)
+  condition<-sapply(strsplit(sapply(strsplit(data_name,split = '_'),function(x) x[2]),'\\.'),function(x) x[1])
   datas<-paste0('profile',condition)
   for(x in seq(1,length(condition))){
     assign(datas[x],read.csv(data_name[x],sep = ' ',col.names=c('Gene',condition[x])))
